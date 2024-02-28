7b3c0498ce1c73d100ed9e279ebe691a0444c7e8
Vyacheslav1557 <slavabirin1@yandex.ru>
2024-02-28 22:46:04 +0500

    feat:

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,14 +1,20 @@
 package app
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/go-chi/httprate"
 	"log/slog"
 	"ms-auth/internal/config"
+	"ms-auth/internal/database/postgresql"
+	"ms-auth/internal/transport/rest/handlers/auth"
+	"ms-auth/internal/transport/rest/handlers/users"
 	"ms-auth/internal/transport/rest/middleware/logger"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
@@ -17,8 +23,12 @@ const (
 	envProd  = "prod"
 )
 
+// TODO: удаление неактивных в течение 60 дней пользователей
+
 func Run() {
-	cfg := config.MustLoad("./configs/config.yaml")
+	config.Init("./configs/config.yaml")
+
+	cfg := config.Cfg()
 
 	log := setupLogger(cfg.Env)
 	log = log.With(slog.String("env", cfg.Env))
@@ -28,39 +38,50 @@ func Run() {
 	)
 	log.Debug("logger debug mode enabled")
 
-	//storage, err := postgres.New(
-	//	fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-	//		cfg.Storage.Host,
-	//		cfg.Storage.Port,
-	//		cfg.Storage.User,
-	//		cfg.Storage.Password,
-	//		cfg.Storage.DBname,
-	//		cfg.Storage.SSLmode,
-	//	),
-	//)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
+	storage, err := postgresql.New(
+		fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			cfg.Storage.Host,
+			cfg.Storage.Port,
+			cfg.Storage.User,
+			cfg.Storage.Password,
+			cfg.Storage.DBname,
+			cfg.Storage.SSLmode,
+		),
+	)
+	if err != nil {
+		panic(err.Error())
+	}
 	//--------------------------------------------R ROUTER--------------------------------------------------------------
 	r := chi.NewRouter()
-	//-------------------------------------------MAIN ROUTER------------------------------------------------------------
-	apiV1Router := chi.NewRouter()
-	apiV1Router.Use(cors.Handler(cors.Options{
+	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Set-Cookie"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
 	}))
-	apiV1Router.Use(middleware.RequestID)
-	apiV1Router.Use(middleware.Logger)
-	apiV1Router.Use(middleware.Recoverer)
-	apiV1Router.Use(middleware.URLFormat)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.URLFormat)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(httprate.LimitByIP(100, 1*time.Minute))
+	//-------------------------------------------AUTH ROUTER------------------------------------------------------------
+	authRouter := chi.NewRouter()
+	authRouter.Post("/login", auth.Login(log, storage))
+	authRouter.Post("/logout", auth.Logout(log, storage))
+	authRouter.Post("/refresh", auth.Refresh(log, storage))
+	//------------------------------------------USERS ROUTER------------------------------------------------------------
+	usersRouter := chi.NewRouter()
+	usersRouter.Post("/", users.NewUser(log, storage))
+	//-------------------------------------------MAIN ROUTER------------------------------------------------------------
+	apiV1Router := chi.NewRouter()
 	apiV1Router.Use(logger.NewLogger(log))
+	apiV1Router.Mount("/auth", authRouter)
+	apiV1Router.Mount("/users", usersRouter)
 	//------------------------------------------------------------------------------------------------------------------
 	r.Mount("/api/v1", apiV1Router)
-
-	err := http.ListenAndServe(cfg.HTTPServer.Host+":"+cfg.HTTPServer.Port, r)
+	err = http.ListenAndServe(cfg.HTTPServer.Host+":"+cfg.HTTPServer.Port, r)
 	if err != nil {
 		log.Error(err.Error())
 	}
